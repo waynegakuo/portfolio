4ddb69ab4c6bfcb3610b878d940d457a57b3bf0e
waynegakuo <waynejotham@gmail.com>
2020-08-02 04:19:45 +0300

    Portofolio v.1 done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# portfolio
+# Personal Portfolio
+
+### Light Mode
+![Portfolio Screenshot-Light Mode](/assets/img/portfolio.png)
+
+### Dark Mode
+![Portfolio Screenshot-Dark Mode](/assets/img/dark_portfolio.png)
+
+This is a simple & responsive personal portfolio that showcases one's but not limited to experiences, education, projects, community work, profile and technical stack. This project is Open Sourced & therefore feel free to clone the repo and create your own portfolio and edit it to your liking. Enjoy! :sparkles: :tada: :octocat:
+
+Done with :heart: by @waynegakuo
--- js/index.js
+++ js/index.js
@@ -7,3 +7,35 @@ function toggleTheme(theme) {
         body.classList.add('light')
     }
 }
+
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+const homeAnchor = document.getElementById('home-anchor');
+const stackAnchor = document.getElementById('stack-anchor');
+const commAnchor = document.getElementById('comm-anchor');
+const eduAnchor = document.getElementById('edu-anchor');
+const expAnchor = document.getElementById('exp-anchor');
+const projAnchor = document.getElementById('proj-anchor');
+
+burger.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+
+homeAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+stackAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+commAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+eduAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+expAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+projAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
