53de18ac047156e4072770a2af0ba21ad558ddc0
Wayne Gakuo <waynejotham@gmail.com>
2026-02-04 13:15:52 +0300

    feat(theme): improve theme toggle logic and update styles for consistency
    - Refactored theme toggle function for better maintainability using `classList`.
    - Updated CSS to fix inconsistent color scheme and enhance readability.
    - Adjusted default theme icon to match the dark mode preference.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 function toggleTheme() {
     const body = document.querySelector("body");
     const icon = document.getElementById("icon");
-    if (body.className === 'dark') {
-        body.className = 'light'
-        icon.className = 'fa fa-moon-o'
+    if (body.classList.contains('light')) {
+        body.classList.remove('light');
+        icon.className = 'fa fa-sun-o'
     }
     else {
-        body.className = 'dark'
-        icon.className = 'fa fa-sun-o'
+        body.classList.add('light');
+        icon.className = 'fa fa-moon-o'
     }
 }
 
